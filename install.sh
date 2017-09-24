e6a2b32dc6a82a1ee4b87cab7f46af606eead2c4
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2017-09-24 17:47:36 +0700

    dont use the vim install script, it's brokey

--- install.sh
+++ install.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install openssh-server net-tools ansible git
+sudo apt-get install git openssh-server
 
 mkdir ~/.ssh
 ssh-keygen -q -f ~/.ssh/id_rsa -N ""
 cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
 
 printf "\n\n"
-printf "You need to enable passwordless sudo, see README.md"
+printf "You need to enable passwordless sudo, see:"
+printf "\thttps://github.com/KMK-Online/workstation/#enable-sudo-password-less"
 printf "\n\n"
-mkdir ~/Workspace
-git clone git://github.com/KMK-ONLINE/workstation.git ~/Workspace/
+
+mkdir -p ~/Workspace/workstation
+git clone git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
+
+sudo pip install -U ansible
+
