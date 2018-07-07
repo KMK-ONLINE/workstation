b365354be793a457817e2272b307a9c944151b28
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2018-07-07 17:51:17 +0700

    checkout ubuntu 18 if you are on 18...

--- install.sh
+++ install.sh
@@ -13,7 +13,12 @@ printf "\thttps://github.com/KMK-Online/workstation/#enable-sudo-password-less"
 printf "\n\n"
 
 mkdir -p ~/Workspace/workstation
-git clone git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
+
+if grep -q 18.04 /etc/lsb-release
+then
+  git clone -b 'ubuntu-18.04' git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
+else
+  git clone git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
+fi
 
 sudo pip install -U ansible
-
