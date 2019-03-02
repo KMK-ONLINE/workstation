7e7aeb1f38f826f409c5cb39843f8a7bfe646841
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2019-03-02 19:08:38 +0700

    ubuntu 18.* is our default now, no need to check

--- install.sh
+++ install.sh
@@ -21,10 +21,4 @@ printf "\thttps://github.com/KMK-Online/workstation/#enable-sudo-password-less"
 printf "\n\n"
 
 mkdir -p ~/Workspace/workstation
-
-if grep -q 18.04 /etc/lsb-release
-then
-  git clone -b 'ubuntu-18.04' git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
-else
-  git clone git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
-fi
+git clone git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
