7cc55fe5dc20e033c3ba7c6c9a0c8d98ebf7d41e
Abror Khoerun Nizam and Enang Yusup <pair+abror+enang@kmklabs.com>
2019-04-04 19:01:07 +0700

    --wip-- [skip ci]

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,14 @@
 Vagrant.configure("2") do |config|
 
   config.vm.box = "ubuntu/bionic64"
+  #config.vm.network :forwarded_port, guest: 22, host: 2226, id: 'ssh'
+  #config.ssh.port = 2226
+  #config.ssh.insert_key = false
+  #config.ssh.private_key_path = ["~/.ssh/id_rsa", "~/.vagrant.d/insecure_private_key"]
+  #config.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "~/.ssh/authorized_keys"
+  #config.ssh.username = "vagrant"
+  #config.ssh.password = "vagrant"
+  #config.ssh.forward_agent = true
 
   config.vm.provision "ansible" do |ansible|
     ansible.playbook = "playbook.yml"
--- install.sh
+++ install.sh
@@ -6,23 +6,23 @@ sudo apt -qq --yes install software-properties-common ansible openssh-server git
 
 printf "\n\n"
 
-mkdir -p ~/.ssh
-if [ ! -f ~/.ssh/id_rsa.pub ]; then
-  ssh-keygen -q -f ~/.ssh/id_rsa -N ""
-else
-  printf "already have ~/.ssh/id_rsa\n"
-fi
+#mkdir -p ~/.ssh
+#if [ ! -f ~/.ssh/id_rsa.pub ]; then
+#  ssh-keygen -q -f ~/.ssh/id_rsa -N ""
+#else
+#  printf "already have ~/.ssh/id_rsa\n"
+#fi
 
-if [ ! -f ~/.ssh/authorized_keys ] || ! grep -q "$(cat ~/.ssh/id_rsa.pub)" ~/.ssh/authorized_keys ; then
-  cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
-else
-  printf "already have ~/.ssh/authorized_keys\n"
-fi
+#if [ ! -f ~/.ssh/authorized_keys ] || ! grep -q "$(cat ~/.ssh/id_rsa.pub)" ~/.ssh/authorized_keys ; then
+#  cat ~/.ssh/id_rsa.pub > ~/.ssh/authorized_keys
+#else
+#  printf "already have ~/.ssh/authorized_keys\n"
+#fi
 
-printf "\n\n"
-printf "You need to enable passwordless sudo, see:\n"
-printf "\thttps://github.com/KMK-Online/workstation/#enable-sudo-password-less"
-printf "\n\n"
+#printf "\n\n"
+#printf "You need to enable passwordless sudo, see:\n"
+#printf "\thttps://github.com/KMK-Online/workstation/#enable-sudo-password-less"
+#printf "\n\n"
 
-mkdir -p ~/Workspace/workstation
-git clone git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
+#mkdir -p ~/Workspace/workstation
+#git clone git://github.com/KMK-ONLINE/workstation.git ~/Workspace/workstation
